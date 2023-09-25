7decd02fe032aa244aba59e1ddeb98ccaefb75ea
mh-mohaghegh75 <hm.mohaghegh@gmail.com>
2023-09-25 23:49:15 +0330

    Feat: OAuth form added

--- routers/auth.py
+++ routers/auth.py
@@ -28,6 +28,18 @@ bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_bearer = OAuth2PasswordBearer(tokenUrl="token")
 
 
+class LoginForm:
+    def __init__(self, request: Request):
+        self.request: Request = request
+        self.username: Optional[str] = None
+        self.password: Optional[str] = None
+
+    async def create_oauth_form(self):
+        form = await  self.request.form()
+        self.username = form.get("email")
+        self.password = form.get("password")
+
+
 class CreateUser(BaseModel):
     username: str
     email: Optional[str]
